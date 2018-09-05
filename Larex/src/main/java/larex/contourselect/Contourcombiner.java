f019f975205249af9a069929c4f97f7cdf1de985
Nico Balbach <NesbiDevelopment@users.noreply.github.com>
2018-09-05 10:39:19 +0200

    Replaced contour merge procedure

--- Larex/src/main/java/larex/contourselect/Contourcombiner.java
+++ Larex/src/main/java/larex/contourselect/Contourcombiner.java
@@ -1,22 +1,53 @@
 package larex.contourselect;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedList;
+import java.util.Comparator;
+import java.util.List;
 
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 public class Contourcombiner {
 
-	public static MatOfPoint combine(Collection<MatOfPoint> contours, Mat source) {
-		LinkedList<Contour> contourList = new LinkedList<>();
+	// Comparator since Lambda not working on some servers
+	private static Comparator<Integer> COMP_INT = new Comparator<Integer>() {
+		@Override public int compare(Integer o1, Integer o2) { return Integer.compare(o1, o2); }
+	}; 
 		
-		for(MatOfPoint contour : contours) {
-			contourList.add(new Contour(contour));
+	public static MatOfPoint combine(Collection<MatOfPoint> contours, Mat source) {
+		if(contours.size() < 1)
+			throw new IllegalArgumentException("Can't combine 0 contours.");
+			
+		List<MatOfPoint> workingContours = new ArrayList<>(contours);
+		
+		while(workingContours.size() > 1) {
+			final List<Integer> heights = new ArrayList<>();
+			final List<Integer> widths = new ArrayList<>();
+			
+			for(MatOfPoint contour: contours) {
+				heights.add(contour.height());
+				widths.add(contour.width());
+			}
+			
+			heights.sort(COMP_INT);
+			widths.sort(COMP_INT);
+			
+			final int medianHeight = heights.get((heights.size()/2));
+			final int medianWidth = widths.get((widths.size()/2));
+			
+			final int thickness = medianHeight < medianWidth ? medianWidth : medianHeight;
+			
+			Mat workingImage = new Mat(source.rows(), source.cols(), CvType.CV_8UC1, new Scalar(0));
+			Imgproc.drawContours(workingImage, new ArrayList<>(contours), -1, new Scalar(255), thickness);
+
+			workingContours = new ArrayList<>(Contourextractor.extract(workingImage));
+			workingImage.release();
 		}
 		
-		Region region = new Region(contourList,source);
-		
-		return region.getRegionMOP();
+		return workingContours.get(0);
 	}
 }
--- Larex/src/main/java/larex/contourselect/Contourextractor.java
+++ Larex/src/main/java/larex/contourselect/Contourextractor.java
@@ -4,19 +4,24 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.imgproc.Imgproc;
 
 public class Contourextractor {
-	public static Collection<MatOfPoint> extract(Mat source) { 
-		Mat tempInverted = new Mat(source.size(), source.type());
+	public static Collection<MatOfPoint> extract(Mat source) {
 		Mat inverted = new Mat(source.size(), source.type());
-		Imgproc.cvtColor(source, tempInverted, Imgproc.COLOR_BGR2GRAY);
-		Imgproc.threshold(tempInverted, inverted, 0, 255, Imgproc.THRESH_OTSU);
-		Core.bitwise_not(inverted, inverted);
-		tempInverted.release();
-		
+		if (source.type() != CvType.CV_8UC1) {
+			Mat tempInverted = new Mat(source.size(), source.type());
+			Imgproc.cvtColor(source, tempInverted, Imgproc.COLOR_BGR2GRAY);
+			Imgproc.threshold(tempInverted, inverted, 0, 255, Imgproc.THRESH_OTSU);
+			Core.bitwise_not(inverted, inverted);
+			tempInverted.release();
+		} else {
+			inverted = source.clone();
+		}
+
 		ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		Imgproc.findContours(inverted, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
