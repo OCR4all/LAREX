e3bc76ced8de284c8b1e5088c854571578d13a56
Nico Balbach <NesbiDevelopment@users.noreply.github.com>
2018-09-05 17:57:38 +0200

    Fixed error by some horizontal divided textblocks

--- Larex/src/main/java/larex/contourselect/Contourcombiner.java
+++ Larex/src/main/java/larex/contourselect/Contourcombiner.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 
-import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
@@ -25,39 +24,42 @@ public class Contourcombiner {
 	};
 
 	public static MatOfPoint combine(Collection<MatOfPoint> contours, Mat source) {
-		return combine(contours, source, 2.5,1.5);
+		return combine(contours, source, 2.5, 1.5);
 	}
 
 	public static MatOfPoint combine(Collection<MatOfPoint> contours, Mat source, double growthY, double growthX) {
 		if (contours.size() < 1)
 			throw new IllegalArgumentException("Can't combine 0 contours.");
 		
-		final Mat workingImage = new Mat(source.rows(), source.cols(), CvType.CV_8UC1, new Scalar(0));
-		Mat temp = workingImage.clone();
 		List<MatOfPoint> workingContours = new ArrayList<>(contours);
+
+		Mat resultImage = new Mat(source.rows(), source.cols(), CvType.CV_8UC1, new Scalar(0));
+		Imgproc.drawContours(resultImage, new ArrayList<>(workingContours), -1, new Scalar(255), -1);
+		Mat workImage = resultImage.clone();
 
 		double growingX = 1;
 		double growingY = 1;
 		int previousContourCount = contours.size();
 		
-		final int cols = workingImage.cols();
-		final int rows = workingImage.rows();
+		final int cols = resultImage.cols();
+		final int rows = resultImage.rows();
 		
 		int top = rows;
 		int bottom = 0;
 		int left = cols;
 		int right = 0;
 		while (workingContours.size() > 1) {
+		//Smear Contours to combine them	
 			//Calc center x and y via moments
 			final List<Integer> heights = new ArrayList<>();
 			final List<Integer> widths = new ArrayList<>();
 			for (MatOfPoint contour : workingContours) {
 				final Rect bounds = Imgproc.boundingRect(contour);
 			
-				top = bounds.y < top ? bounds.y : top; 
-				bottom = bounds.br().y > bottom ? (int) bounds.br().y : bottom; 
-				left = bounds.x < left ? bounds.x : left; 
-				right = bounds.br().x > right ? (int) bounds.br().x : right; 
+				top = bounds.y <= top ? bounds.y -1 : top; 
+				bottom = bounds.br().y >= bottom ? (int) bounds.br().y +1: bottom; 
+				left = bounds.x <= left ? bounds.x -1: left; 
+				right = bounds.br().x >= right ? (int) bounds.br().x +1: right; 
 
 				heights.add(bounds.height);
 				widths.add(bounds.width);
@@ -69,18 +71,14 @@ public class Contourcombiner {
 
 			final double medianDistanceX = widths.get(widths.size()/2)*growingX;
 			final double medianDistanceY = heights.get(heights.size()/2)*growingY;
-			
-			//Smear Contours to combine them	
-			Imgproc.drawContours(workingImage, new ArrayList<>(workingContours), -1, new Scalar(255), -1);
-
 
 			// Smearing
 			int[] currentGapsX = new int[rows];
-			Arrays.fill(currentGapsX, Integer.MAX_VALUE);
+			Arrays.fill(currentGapsX, ((int)medianDistanceX)+1);
 			for (int x = left; x <= right; x++) {
-				int currentGapY = Integer.MAX_VALUE;
+				int currentGapY =  ((int)medianDistanceY)+1;
 				for (int y = top; y <= bottom; y++) {
-					double value = temp.get(y, x)[0];
+					double value = workImage.get(y, x)[0];
 					if (value > 0) {
 						// Entered Contour
 						final int currentGapX = currentGapsX[y];
@@ -88,51 +86,52 @@ public class Contourcombiner {
 						if (currentGapY < medianDistanceY) {
 							// Draw over
 							for (int i = 1; i <= currentGapY; i++)
-								workingImage.put(y - i,x, new byte[] { 1 });
+								resultImage.put(y - i,x, new double[] {255});
 						}
 
 						if (currentGapX < medianDistanceX) {
+							if(currentGapX > 0)
 							// Draw over
 							for (int i = 1; i <= currentGapX; i++)
-								workingImage.put(y,x - i, new byte[] { 1 });
+								resultImage.put(y,x - i, new double[] {255});
 						}
 						
 						currentGapY = 0;
-						currentGapsX[x] = 0;
+						currentGapsX[y] = 0;
 					} else {
 						// Entered/Still in Gap
 						currentGapY++;
-						currentGapsX[x]++;
+						currentGapsX[y]++;
 					}
 				}
 			}
 
 
-			workingContours = new ArrayList<>(Contourextractor.extract(workingImage));
+			workingContours = new ArrayList<>(Contourextractor.extract(resultImage));
 			int contourCount = workingContours.size();
 			
 			if(previousContourCount == contourCount) {
 				growingX = growingX*growthX;
 				growingY = growingY*growthY;
-			}else {
+			} else {
 				growingX = 1;
 				growingY = 1;
 			}
 			
 			//Copy current to temp
 			for (int x = left; x <= right; x++) 
-				for (int y = top; y <= bottom; y++){
-					if (workingImage.get(y, x)[0] > 0) temp.put(y,x,1);
-					else temp.put(y, x, 0);
+				for (int y = top; y <= bottom; y++)
+					if (resultImage.get(y, x)[0] > 0) workImage.put(y,x,new double[] {255});
+					else workImage.put(y, x, new byte[] {0});
 				
 			previousContourCount = contourCount;
 		}
 
 		//Draw small border to account for shrinking
-		Imgproc.drawContours(workingImage, new ArrayList<>(workingContours), -1, new Scalar(255), 2);
-		workingContours = new ArrayList<>(Contourextractor.extract(workingImage));
-		workingImage.release();
-		temp.release();
+		Imgproc.drawContours(resultImage, new ArrayList<>(workingContours), -1, new Scalar(255), 2);
+		workingContours = new ArrayList<>(Contourextractor.extract(resultImage));
+		resultImage.release();
+		workImage.release();
 		System.gc();
 
 		return workingContours.get(0);
