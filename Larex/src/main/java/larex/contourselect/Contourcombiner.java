20b26116de8422949297efccb18f7c9415ac655a
Nico Balbach <NesbiDevelopment@users.noreply.github.com>
2018-09-05 12:40:20 +0200

    Added smearing algorithm for contour combine

--- Larex/src/main/java/larex/contourselect/Contourcombiner.java
+++ Larex/src/main/java/larex/contourselect/Contourcombiner.java
@@ -8,46 +8,130 @@ import java.util.List;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 public class Contourcombiner {
 
 	// Comparator since Lambda not working on some servers
-	private static Comparator<Integer> COMP_INT = new Comparator<Integer>() {
-		@Override public int compare(Integer o1, Integer o2) { return Integer.compare(o1, o2); }
-	}; 
-		
+	private static Comparator<Double> COMP_DOUBLE = new Comparator<Double>() {
+		@Override
+		public int compare(Double o1, Double o2) {
+			return Double.compare(o1, o2);
+		}
+	};
+
 	public static MatOfPoint combine(Collection<MatOfPoint> contours, Mat source) {
-		if(contours.size() < 1)
+		return combine(contours, source, 3);
+	}
+
+	public static MatOfPoint combine(Collection<MatOfPoint> contours, Mat source, double grow) {
+		if (contours.size() < 1)
 			throw new IllegalArgumentException("Can't combine 0 contours.");
-			
-		List<MatOfPoint> workingContours = new ArrayList<>(contours);
 		
-		while(workingContours.size() > 1) {
-			final List<Integer> heights = new ArrayList<>();
-			final List<Integer> widths = new ArrayList<>();
-			
-			for(MatOfPoint contour: contours) {
-				heights.add(contour.height());
-				widths.add(contour.width());
+		final Mat workingImage = new Mat(source.rows(), source.cols(), CvType.CV_8UC1, new Scalar(0));
+		List<MatOfPoint> workingContours = new ArrayList<>(contours);
+
+		double growing = 1;
+		int previousContourCount = contours.size();
+		
+		while (workingContours.size() > 1) {
+			//Calc center x and y via moments
+			final List<Double> centersY = new ArrayList<>();
+			final List<Double> centersX = new ArrayList<>();
+			for (MatOfPoint contour : workingContours) {
+				final Moments moments= Imgproc.moments(contour);
+				final Point centroid = new Point();
+				centroid.x = moments.get_m10() / moments.get_m00();
+				centroid.y = moments.get_m01() / moments.get_m00();	
+				
+				centersY.add(centroid.y);
+				centersX.add(centroid.x);
+			}
+
+			//Find median distance between contour moments
+			centersY.sort(COMP_DOUBLE);
+			centersX.sort(COMP_DOUBLE);
+
+			final List<Double> distancesX = new ArrayList<>();
+			final List<Double> distancesY = new ArrayList<>();
+			for(int i = 0; i < workingContours.size() - 1; i++) {
+				distancesX.add(centersX.get(i+1) - centersX.get(i));
+				distancesY.add(centersY.get(i+1) - centersY.get(i));
 			}
 			
-			heights.sort(COMP_INT);
-			widths.sort(COMP_INT);
+			double minDistanceY = distancesX.get(distancesX.size()/2) * growing;
+			double minDistanceX = distancesY.get(distancesY.size()/2) * growing;
 			
-			final int medianHeight = heights.get((heights.size()/2));
-			final int medianWidth = widths.get((widths.size()/2));
-			
-			final int thickness = medianHeight < medianWidth ? medianWidth : medianHeight;
-			
-			Mat workingImage = new Mat(source.rows(), source.cols(), CvType.CV_8UC1, new Scalar(0));
-			Imgproc.drawContours(workingImage, new ArrayList<>(contours), -1, new Scalar(255), thickness);
+			//Smear Contours to combine them	
+			Imgproc.drawContours(workingImage, new ArrayList<>(workingContours), -1, new Scalar(255), -1);
+
+			Mat temp = workingImage.clone();
+
+			// Vertical Smearing
+			for (int x = 0; x < workingImage.rows(); x++) {
+				int currentGap = 0;
+				for (int y = 0; y < workingImage.cols(); y++) {
+					double value = temp.get(x, y)[0];
+					if (value > 0) {
+						// Entered Contour
+						if (currentGap < minDistanceY) {
+							// Draw over
+							for (int i = 1; i <= currentGap; i++)
+								workingImage.put(x, y - i, new byte[] { 1 });
+						}
+						currentGap = 0;
+					} else {
+						// Entered/Still in Gap
+						currentGap++;
+					}
+				}
+			}
+
+			// Horizontal smearing
+			for (int y = 0; y < workingImage.cols(); y++) {
+				int currentGap = 0;
+				for (int x = 0; x < workingImage.rows(); x++) {
+					double value = temp.get(x, y)[0];
+					if (value > 0) {
+						// Entered Contour
+						if (currentGap < minDistanceX) {
+							// Draw over
+							for (int i = 1; i <= currentGap; i++)
+								workingImage.put(x - i, y, new byte[] { 1 });
+						}
+						currentGap = 0;
+					} else {
+						// Entered/Still in Gap
+						currentGap++;
+					}
+
+				}
+			}
+
+			temp.release();
 
 			workingContours = new ArrayList<>(Contourextractor.extract(workingImage));
-			workingImage.release();
+			int contourCount = workingContours.size();
+			
+			if(previousContourCount == contourCount) {
+				growing = growing*grow;
+			}else {
+				growing = 1;
+			}
+			
+			previousContourCount = contourCount;
+			
+			if(workingContours.size() > 1) {
+				//Draw small border to account for shrinking
+				Imgproc.drawContours(workingImage, new ArrayList<>(workingContours), -1, new Scalar(255), 1);
+				workingContours = new ArrayList<>(Contourextractor.extract(workingImage));
+			}
 		}
-		
+		workingImage.release();
+
 		return workingContours.get(0);
 	}
 }
--- Larex/src/main/java/larex/contourselect/Contourextractor.java
+++ Larex/src/main/java/larex/contourselect/Contourextractor.java
@@ -16,12 +16,13 @@ public class Contourextractor {
 			Mat tempInverted = new Mat(source.size(), source.type());
 			Imgproc.cvtColor(source, tempInverted, Imgproc.COLOR_BGR2GRAY);
 			Imgproc.threshold(tempInverted, inverted, 0, 255, Imgproc.THRESH_OTSU);
-			Core.bitwise_not(inverted, inverted);
 			tempInverted.release();
+			Core.bitwise_not(inverted, inverted);
 		} else {
 			inverted = source.clone();
 		}
 
+			
 		ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		Imgproc.findContours(inverted, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
