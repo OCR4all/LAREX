d75519c034582f04f837e8b0f2594c49c09969ae
Nico Balbach <Nico.Balbach@gmail.com>
2019-08-20 11:46:40 +0200

    Added static resources max age

--- src/main/java/com/web/config/MvcConfiguration.java
+++ src/main/java/com/web/config/MvcConfiguration.java
@@ -1,8 +1,11 @@
 package com.web.config;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -44,6 +47,7 @@ public class MvcConfiguration implements WebMvcConfigurer {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/")
+		.setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).cachePublic());
 	}
 }
