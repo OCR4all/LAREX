6acaa75ed02f324be931d19a99f5be516810b573
maxnth <maximilian.noeth@protonmail.com>
2021-06-05 14:50:50 +0200

    add build environment for development

---
+++ development/build.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+####################################################################
+######## Build and run a development Docker image for Larex ########
+####################################################################
+
+## Container name (must be lower case)
+DOCKER_NAME="larex_dev"
+
+## Image name
+IMAGE_NAME="larex_dev"
+
+## Local path to the directory containing the books (must be changed to the actual book directory location!)
+LAREX_DIR="/Path/to/books"
+
+## Local path to the custom configuration file
+CONFIG_FILE="${PWD}/dev.config"
+
+## Port
+PORT=5555
+
+### Build LAREX
+mvn clean install -f ../pom.xml
+cp ../target/Larex.war .
+
+## Stop and delete old container (if exists)
+OLD="$(docker ps --all --quiet --filter=name="$DOCKER_NAME")"
+if [[ -n "$OLD" ]]; then
+  docker stop ${OLD} && docker rm ${OLD}
+fi
+
+## Build and start container
+docker build -t ${IMAGE_NAME} . && \
+docker run \
+    -p ${PORT}:8080 \
+    --name ${DOCKER_NAME} \
+    -v ${CONFIG_FILE}:/larex.config \
+    -v ${LAREX_DIR}:/home/books/ \
+    -it ${IMAGE_NAME}
