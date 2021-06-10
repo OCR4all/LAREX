af731fc45509185c3d45b1b146e7d27d0821b138
maxnth <maximilian.noeth@protonmail.com>
2021-06-10 23:28:44 +0200

    change error 500 message

--- src/main/java/de/uniwue/web/controller/ErrorController.java
+++ src/main/java/de/uniwue/web/controller/ErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Communication Controller to handle error pages
- * 
+ *
  */
 @Controller
 public class ErrorController {
@@ -19,14 +19,14 @@ public class ErrorController {
 		model.addAttribute("code", "400");
 		return "error";
 	}
-	
+
 	@RequestMapping(value = "/error/403")
 	public String error403(Model model) {
 		model.addAttribute("message", "We are sorry but you are not authorized to use the requested page.");
 		model.addAttribute("code", "403");
 		return "error";
 	}
-	
+
 	@RequestMapping(value = "/error/404")
 	public String error404(Model model) {
 		model.addAttribute("message", "We can't seem to find the page you're looking for.");
@@ -36,7 +36,7 @@ public class ErrorController {
 
 	@RequestMapping(value = "/error/500")
 	public String error500(Model model) throws IOException {
-		model.addAttribute("message", "It seems like the server has mixed something up.");
+		model.addAttribute("message", "It seems like the server has mixed something up. Please check whether the book directory is accessible to LAREX and whether the directory follows the necessary structure.");
 		model.addAttribute("code", "500");
 		return "error";
 	}
