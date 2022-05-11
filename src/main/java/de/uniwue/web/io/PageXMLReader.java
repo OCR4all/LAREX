98b1d4fc686d7cb210e9e6da32cc8b0c2f300c01
maxnth <maximilian.noeth@protonmail.com>
2022-05-11 12:41:05 +0200

    check whether word @confidence exists before appending word element
    Signed-off-by: maxnth <maximilian.noeth@protonmail.com>

--- src/main/java/de/uniwue/web/io/PageXMLReader.java
+++ src/main/java/de/uniwue/web/io/PageXMLReader.java
@@ -134,11 +134,16 @@ public class PageXMLReader {
 										Glyph primaGlyph = (Glyph) primaWord.getTextObject(j);
 										glyphs.add(new GlyphContainer(primaGlyph, emptyPolygon));
 									}
-									words.add(new de.uniwue.web.model.Word(primaWord.getId().toString(),
-													emptyPolygon,
-													primaWord.getText(),
-													primaWord.getConfidence(),
-													glyphs));
+
+									/* Only append Word-elements with confidence as LAREX currently only uses the word confidence.
+									Has to be changed as soon as LAREX also displays word coords etc. */
+									if(primaWord.getConfidence() != null){
+										words.add(new de.uniwue.web.model.Word(primaWord.getId().toString(),
+												emptyPolygon,
+												primaWord.getText(),
+												primaWord.getConfidence(),
+												glyphs));
+									}
 								}
 							}
 
